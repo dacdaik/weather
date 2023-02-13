0ba916197d4eab98fc7f1f5d23a69396dc179c48
dacdaik <caobadacck6@g,mail.com>
2023-02-14 00:05:20 +0700

    14.2

--- src/Componets/ChooseState/index.js
+++ src/Componets/ChooseState/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import cities from "../../Data/in.json";
+import {UseWeaterAPPContext} from "../../Context/Context"
 
 const ChooseStateComponents = () => {
+
+    const {state:{city}, dispatch} = UseWeaterAPPContext();
+    console.log('UseWeaterAPPContext', UseWeaterAPPContext());
+
+    const handleChange = (e) => {
+        const selectedCity = cities.filter((city)=>{
+            return city.city === e.target.value
+        })[0]
+        console.log(selectedCity);
+    }
     return (
         <>
         <div className="stateWrap">
-            <select className="stateMenu">
+            <select className="stateMenu" defaultValue={city} onChange={handleChange}>
                 {
                     cities && cities.length > 0 && cities.map((city)=>{
                         return (
--- src/Context/Context.js
+++ src/Context/Context.js
@@ -6,7 +6,17 @@ const WeaterAPPContext = React.createContext();
 
 const WeaterAPPProvider = ({children}) => {
     const [state, dispatch] = useReducer(WeatherReducer,{
-        city: '',
+        city: {
+            "city": "Delhi", 
+            "lat": "28.6600", 
+            "lng": "77.2300", 
+            "country": "India", 
+            "iso2": "IN", 
+            "admin_name": "Delhi", 
+            "capital": "admin", 
+            "population": "29617000", 
+            "population_proper": "16753235"
+          },
         current:'',
         daily:''
     })
